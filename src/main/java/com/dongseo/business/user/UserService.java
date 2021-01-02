62d2f76c0ca7437f1eefb75dabfca96ea133cc26
DESKTOP-T80EHS0\user <kang815595@gmail.com>
2021-01-02 13:08:35 +0900

    유저 서비스, 서비스impl 제작시작

--- src/main/java/com/dongseo/business/user/UserService.java
+++ src/main/java/com/dongseo/business/user/UserService.java
@@ -1,4 +1,8 @@
 package com.dongseo.business.user;
 
 public interface UserService {
+    void insertUser(UserVo vo);
+    void updateUser(UserVo vo);
+    void deleteUser(UserVo vo);
+    UserVo getUser(UserVo vo);
 }
---
+++ src/main/java/com/dongseo/business/user/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.dongseo.business.user.impl;
+
+import com.dongseo.business.user.UserService;
+import com.dongseo.business.user.UserVo;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void insertUser(UserVo vo) {
+
+    }
+
+    @Override
+    public void updateUser(UserVo vo) {
+
+    }
+
+    @Override
+    public void deleteUser(UserVo vo) {
+
+    }
+
+    @Override
+    public UserVo getUser(UserVo vo) {
+        return null;
+    }
+}
